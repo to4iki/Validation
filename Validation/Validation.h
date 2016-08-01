d6d86bd8a85c5a90d622aa591fff3251c5352c98
tsk takezawa <tsk.take815@gmail.com>
2016-08-02 00:00:24 +0900

    Initial Commit

---
+++ Validation/Validation.h
@@ -0,0 +1,19 @@
+//
+//  Validation.h
+//  Validation
+//
+//  Created by to4iki on 7/28/16.
+//  Copyright © 2016 to4iki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Validation.
+FOUNDATION_EXPORT double ValidationVersionNumber;
+
+//! Project version string for Validation.
+FOUNDATION_EXPORT const unsigned char ValidationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Validation/PublicHeader.h>
+
+
